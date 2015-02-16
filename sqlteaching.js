adeca1c42714a3463d62c24e4daa04fc48fb4704
Ronald Cheng <rhc2104@columbia.edu>
2015-02-15 16:18:53 -0800

    no trailing comma

--- sqlteaching.js
+++ sqlteaching.js
@@ -137,7 +137,7 @@ var levels = [{'name': 'SELECT *',
                'answer': {'columns': ['id', 'name', 'birthdate'],
                           'values': [[2, 'Justin Timberlake', '1981-01-31'],
                                      [3, 'Taylor Swift', '1989-12-13']]},
-               'prompt': 'Sometimes, a column can contain a date value.  The first 4 digits represents the year, the next 2 digits represents the month, and the next 2 digits represents the day of the month.  For example, <code>1985-07-20</code> would mean July 20, 1985.<br/><br/>You can compare dates by using <code><</code> and <code>></code>.  For example, <code>SELECT * FROM celebs_born WHERE birthdate < \'1985-08-17\';</code> returns a list of celebrities that were born before August 17th, 1985.<br/><br/>Can you return a list of celebrities that were born after September 1st, 1980?'},
+               'prompt': 'Sometimes, a column can contain a date value.  The first 4 digits represents the year, the next 2 digits represents the month, and the next 2 digits represents the day of the month.  For example, <code>1985-07-20</code> would mean July 20, 1985.<br/><br/>You can compare dates by using <code><</code> and <code>></code>.  For example, <code>SELECT * FROM celebs_born WHERE birthdate < \'1985-08-17\';</code> returns a list of celebrities that were born before August 17th, 1985.<br/><br/>Can you return a list of celebrities that were born after September 1st, 1980?'}
               ];
 
 
